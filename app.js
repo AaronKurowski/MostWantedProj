37b83fdff8c9e128f3339f8eff4cdeb033a2a531
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-09 10:23:14 -0400

    Accessed each object property in the searchByHeight function to display object contents to the DOM. Appended table data to table row.

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ function searchByName(){
         document.getElementById(
             "output"
         ).innerHTML = `${firstNameInput}`;
+        console.log(filteredPeople);
     }else{
         console.log('Sorry, looks like there is no one with that name.');
     }
@@ -58,15 +59,26 @@ function searchByBirth() {
 }
 
 function searchByHeight() {
+    let tableData = document.createElement('td');
+    let tableRow = document.getElementById("tableRow");
     let heightInput = document.forms['heightForm']['height'].value;
     for(let i = 0; i < people.length; i++) {
         if(heightInput == people[i].height) {
             console.log(people[i].firstName, people[i].lastName, people[i].height);
-            document.getElementById("height-output").innerHTML += `${people[i].firstName} ${people[i].lastName} ${people[i].height} `;
+            tableData.innerHTML += `${people[i].id} ${people[i].firstName} ${people[i].lastName} ${people[i].gender} ${people[i].dob} ${people[i].height} ${people[i].weight} ${people[i].eyeColor} ${people[i].occupation} ${people[i].parents} ${people[i].currentSpouse}`;
+            tableRow.appendChild(tableData);
         } else {
             console.log("No match for this height found");
         }
     }
+}
+
+let thData = document.createElement("th");
+let tableHeaders = ['Id', 'First Name', 'Last Name', 'Gender', 'DOB', 'Height', 'Weight', 'Eye Color', 'Occupation', 'Parents', 'Current Spouse'];
+for(let i = 0; i < tableHeaders.length; i++) {
+    //use the += in a for loop to show value + the next value of the array
+    thData.innerHTML += " " + tableHeaders[i];
+    document.getElementById("thead").appendChild(thData);
 }
 
 
