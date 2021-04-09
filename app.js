0beedbefe17596b444df245217efad1605eb1882
Aaron Kurowski <aaron12342@att.net>
2021-04-09 10:40:39 -0500

    created search by birth function and a table to accept people data.

--- app.js
+++ app.js
@@ -41,13 +41,8 @@ function searchByGender(){
     }
 }
 
-<<<<<<< HEAD
-function searchByBirth(){
-    let birthdayInput = document.forms['birthForm']['birthday'].value;
-=======
 function searchByBirth() {
     let birthdayInput = document.forms['birthdayForm']['birthday'].value;
->>>>>>> 37b83fdff8c9e128f3339f8eff4cdeb033a2a531
 
     let filteredBirthdays = people.filter(function (person) {
         if(person.dob === birthdayInput){
@@ -61,31 +56,4 @@ function searchByBirth() {
         console.log("No one with that birthday.")
     }
 }
-<<<<<<< HEAD
-=======
-
-function searchByHeight() {
-    let tableData = document.createElement('td');
-    let tableRow = document.getElementById("tableRow");
-    let heightInput = document.forms['heightForm']['height'].value;
-    for(let i = 0; i < people.length; i++) {
-        if(heightInput == people[i].height) {
-            console.log(people[i].firstName, people[i].lastName, people[i].height);
-            tableData.innerHTML += `${people[i].id} ${people[i].firstName} ${people[i].lastName} ${people[i].gender} ${people[i].dob} ${people[i].height} ${people[i].weight} ${people[i].eyeColor} ${people[i].occupation} ${people[i].parents} ${people[i].currentSpouse}`;
-            tableRow.appendChild(tableData);
-        } else {
-            console.log("No match for this height found");
-        }
-    }
-}
-
-let thData = document.createElement("th");
-let tableHeaders = ['Id', 'First Name', 'Last Name', 'Gender', 'DOB', 'Height', 'Weight', 'Eye Color', 'Occupation', 'Parents', 'Current Spouse'];
-for(let i = 0; i < tableHeaders.length; i++) {
-    //use the += in a for loop to show value + the next value of the array
-    thData.innerHTML += " " + tableHeaders[i];
-    document.getElementById("thead").appendChild(thData);
-}
-
->>>>>>> 37b83fdff8c9e128f3339f8eff4cdeb033a2a531
 
